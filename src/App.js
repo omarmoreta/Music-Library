999b59a7c0477607031bf3f417f402a0684d269f
Omar <Omar>
2022-02-02 00:15:43 -0500

    added a handleSearch function and fixed hard coded API

--- src/App.js
+++ src/App.js
@@ -8,28 +8,35 @@ function App() {
   let [message, setMessage] = useState("Search for Music!");
   let [data, setData] = useState([]);
 
+  const API_URL = "https://itunes.apple.com/search?term=";
+
   useEffect(() => {
-    const fetchData = async () => {
-      document.title = `${search} Music`;
-      const response = await fetch(
-        "https://itunes.apple.com/search?term=the%20grateful%20dead"
-      );
-      const resData = await response.json();
-      if (resData.results.length > 0) {
-        setData(resData.results);
-      } else {
-        setMessage("Not Found");
-      }
-      console.log(resData);
-    };
-    fetchData();
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`;
+        const response = await fetch(API_URL + search);
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          setData(resData.results);
+        } else {
+          setMessage("Not Found");
+        }
+        console.log(resData);
+      };
+      fetchData();
+    }
   }, [search]);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
 
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery />
+      <Gallery data={data} />
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,8 +4,12 @@ function SearchBar(props) {
   let [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <form>
-      <input type="text" placeholder="Enter a search term here" />
+    <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+      <input
+        type="text"
+        placeholder="Enter a search term here"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <input type="submit" />
     </form>
   );
