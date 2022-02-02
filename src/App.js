62169c7fdd8f6fda5e7ff92d9d5d750a72c790a4
Omar <Omar>
2022-02-01 23:57:55 -0500

    added useEffect and fetch to Itunes API

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ function App() {
   let [message, setMessage] = useState("Search for Music!");
   let [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`;
+      const response = await fetch(
+        "https://itunes.apple.com/search?term=the%20grateful%20dead"
+      );
+      const resData = await response.json();
+      if (resData.results.length > 0) {
+        setData(resData.results);
+      } else {
+        setMessage("Not Found");
+      }
+      console.log(resData);
+    };
+    fetchData();
+  }, [search]);
+
   return (
     <div className="App">
       <SearchBar />
